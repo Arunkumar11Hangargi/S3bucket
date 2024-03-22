1f4a277ff08bb22c5f5e2524dd66441a84e93877
Arunkumar11Hangargi <164481294+Arunkumar11Hangargi@users.noreply.github.com>
2024-03-22 14:47:02 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"  # Specify the AWS region you want to use
+  region = "us-east-1" 
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Specify a unique name for your bucket
+  bucket = "s3_prod"
   acl    = "private" 
 }
