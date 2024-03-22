931b295f6863dfccb5e42e8bf7fd48d20f23ca93
Arunkumar11Hangargi <164481294+Arunkumar11Hangargi@users.noreply.github.com>
2024-03-22 14:43:30 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,36 +1,8 @@
 provider "aws" {
-  region = "us-west-2" # specify the AWS region you want to use
+  region = "us-east-1"  # Specify the AWS region you want to use
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name" # specify a unique name for your bucket
-
-  # Uncomment the below line if you want to enable versioning for the bucket
-  # versioning {
-  #   enabled = true
-  # }
-
-  # Uncomment the below block if you want to configure server-side encryption
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       sse_algorithm = "AES256" # You can also use "aws:kms" for KMS encryption
-  #     }
-  #   }
-  # }
-
-  # Uncomment the below block if you want to configure bucket ACL (Access Control List)
-  # acl = "private" # You can use private, public-read, public-read-write, or authenticated-read
-
-  # Uncomment the below block if you want to configure bucket tags
-  # tags = {
-  #   Name        = "MyS3Bucket"
-  #   Environment = "Production"
-  # }
-
-  # Uncomment the below block if you want to enable logging for the bucket
-  # logging {
-  #   target_bucket = "logs-bucket"
-  #   target_prefix = "s3-logs/"
-  # }
+  bucket = "my-unique-bucket-name"  # Specify a unique name for your bucket
+  acl    = "private" 
 }
